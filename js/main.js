d8be87632a8cebc828ac25eb9d37bc8df789b4cf
Stin3X <st1n3x666@gmail.com>
2021-04-15 22:17:04 +0300

    v2.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 100,
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
     },
 });
